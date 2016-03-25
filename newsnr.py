62538fad1e106a5ce8caf389cfd6a3c23d8e2d8b
Pedro Henrique Bernardinelli <pedro.h.bernardinelli@gmail.com>
2016-03-25 10:18:42 -0300

    Added also a function that marginalizes for SNR, so a comparison between J-PAS and SDSS becomes reasonable.

--- newsnr.py
+++ newsnr.py
@@ -1,3 +1,31 @@
+'''
+Author: Pedro Bernardinelli (pedro.h.bernardinelli@gmail.com)
+Date: Mar. 25, 2016
+
+This short code gives an estimation of the survey dependent part of the Fisher matrix in a given supernovae Ia simulation in bins of redshift and
+signal-to-noise ratio. 
+
+Usage: call the analyse_and_plot(data_filte, plotformat, scaling) function. 
+data_file is a file with the redshift z, the signal-to-noise ratio snr and the error in the distance modulus mu for each object. 
+A complementary shell script file is provided for transforming SALT2mu's fitres files into the required format for this code. 
+plotformat defines how the style of the lines of the plot. pylab/matplotlib accepts "--", ".-", "-", ":", ".", among others. 
+Check http://matplotlib.org/1.3.1/examples/pylab_examples/line_styles.html for more information.
+scaling is a parameter for scaling the amplitude of the Fisher matrix. 
+A simple way to do use this parameter would be the number of objects per season divided by the number of simulated objects, or the purity and contamination
+of the sample coming from photometric classification.
+
+The function also returns dN/dz*\sigma_\mu^{-2} marginalized over the SNR distribution. This is interesting for comparing two 
+simulations with very different ranges of redshifts (for example, SDSS and J-PAS). 
+The marginalized_plot(marginalization, plotformat, curve_label) function plots this for the average z in each z bin. 
+
+
+Warning: the values of each bin are only to be compared within other values for the same bin. Don't compare one bin to another. This happens because 
+the full Fisher matrix has a cosmology dependent parameter that is not included here.
+
+'''
+
+
+
 import numpy as np
 import pylab as pl
 
@@ -19,12 +47,12 @@ def SNRbin(SNRmin,SNRmax,SNR,sigma):
 		sigmabin=[]
 		while i < len(SNR):
 			if SNR[i] < SNRmax and SNR[i] > SNRmin:
-				sigmabin.append(sigma[i])
+				sigmabin.append(sigma[i]**-2.)
 			i = i + 1
 
 		SNRav = (SNRmin+SNRmax)*0.5
 		sig = np.average(sigmabin)
-		A = len(sigmabin)*(sig**-2)/0.05
+		A = len(sigmabin)*(sig)/0.05
 		return A,SNRav
 
 def findtheparameter(SNRmin,SNRmax,zmin,zmax,z,snr, sigma):
@@ -36,6 +64,7 @@ def findtheparameter(SNRmin,SNRmax,zmin,zmax,z,snr, sigma):
 def analyse_and_plot(data_file, plotformat, scaling):
 	z,snr,sigma = np.loadtxt(data_file, unpack=True)
 
+	marginalization = []
 	SNRbinsize=1
 
 	maxSNR = np.amax(snr)
@@ -51,6 +80,8 @@ def analyse_and_plot(data_file, plotformat, scaling):
 	pl.subplot(221)
 	pl.ylabel(r"$\frac{\mathrm{d} N_z}{\mathrm{d} z} \sigma_\mu^{-2} $",fontsize=15)
 	pl.plot(SNRdat,Adat,'b' + plotformat,label="$0.10<z<0.15$")
+	marginalization.append(np.nansum(Adat))
+
 
 	SNRi=0
 	Adat=[]
@@ -63,6 +94,8 @@ def analyse_and_plot(data_file, plotformat, scaling):
 	pl.subplot(221)
 	pl.ylabel(r"$\frac{\mathrm{d} N_z}{\mathrm{d} z} \sigma_\mu^{-2} $",fontsize=15)
 	pl.plot(SNRdat,Adat,'r' + plotformat,label="$0.15<z<0.20$")
+	marginalization.append(np.nansum(Adat))
+
 
 	legend = pl.legend(loc=0, shadow=True,fontsize=15)
 
@@ -77,6 +110,8 @@ def analyse_and_plot(data_file, plotformat, scaling):
 		SNRi = SNRi + SNRbinsize
 	pl.subplot(222)
 	pl.plot(SNRdat,Adat,'g' + plotformat,label='$0.20<z<0.25$')
+	marginalization.append(np.nansum(Adat))
+
 
 
 	SNRi=0
@@ -90,6 +125,8 @@ def analyse_and_plot(data_file, plotformat, scaling):
 	pl.subplot(222)
 	pl.plot(SNRdat,Adat,'m' + plotformat,label='$0.25<z<0.30$')
 	legend = pl.legend(loc=0, shadow=True,fontsize=15)
+	marginalization.append(np.nansum(Adat))
+
 
 	SNRi=0
 	Adat=[]
@@ -103,6 +140,8 @@ def analyse_and_plot(data_file, plotformat, scaling):
 	pl.xlabel("$SNR$",fontsize=15)
 	pl.ylabel(r"$\frac{\mathrm{d} N_z}{\mathrm{d} z} \sigma_\mu^{-2} $",fontsize=15)
 	pl.plot(SNRdat, Adat,'c' + plotformat,label='$0.30<z<0.35$')
+	marginalization.append(np.nansum(Adat))
+
 
 
 	SNRi=0
@@ -117,6 +156,8 @@ def analyse_and_plot(data_file, plotformat, scaling):
 	pl.xlabel("$SNR$")
 	pl.plot(SNRdat,Adat,'k' + plotformat,label="$0.35<z<0.40$")
 	legend = pl.legend(loc=0, shadow=True,fontsize=15)
+	marginalization.append(np.nansum(Adat))
+
 
 
 	SNRi=0
@@ -130,14 +171,31 @@ def analyse_and_plot(data_file, plotformat, scaling):
 	pl.subplot(224)
 	pl.plot(SNRdat,Adat,'m' + plotformat,label='$0.40<z<0.45$')
 	pl.xlabel("$SNR$",fontsize=15)
+	marginalization.append(np.nansum(Adat))
+
+	return marginalization
 
 '''
 -------------------------------------------------------------------------------------------------------------------------------------------------------------------
 '''
 
-analyse_and_plot("jplus.log", "--", 1)
+def marginalized_plot(marginalization, plotformat, curve_label):
+	zbins = [0.125, 0.175, 0.225, 0.275, 0.325, 0.375, 0.425]
 
-analyse_and_plot("jpas.log", "-", 2)
+	pl.plot(zbins, marginalization, plotformat, label=curve_label)
+	pl.xlabel("$z$",fontsize=15)
+	pl.ylabel(r"$\sum _i\frac{\mathrm{d} N_z}{\mathrm{d} z} \sigma_\mu^{-2} SNR_i$",fontsize=15)
+
+'''
+-------------------------------------------------------------------------------------------------------------------------------------------------------------------
+'''
+jpas_mar = analyse_and_plot("jpas.log", "-", 227./10000.)
+jpas_double_mar = analyse_and_plot("jpas.log", ":", 2*227./10000.)
+jpas_3k_mar = analyse_and_plot("jpas.log", ":", 908./10000.)
+
+
+sdss_mar = analyse_and_plot("SDSS.log", "--", 373./10000.)
+
 
 pl.suptitle(r'Information', fontsize=15)
 legend = pl.legend(loc=0, shadow=True)
@@ -145,3 +203,17 @@ frame = legend.get_frame()
 fig = pl.gcf()
 fig.set_size_inches(15,10)
 pl.savefig("jpas.pdf")
+
+pl.clf()
+
+marginalized_plot(jpas_mar, '.-', curve_label="J-PAS 750 sq deg")
+marginalized_plot(jpas_double_mar, '.-', curve_label="J-PAS 1500 sq deg")
+marginalized_plot(jpas_3k_mar, '.-', curve_label="J-PAS 3000 sq deg")
+marginalized_plot(sdss_mar, '.-', curve_label="SDSS")
+
+pl.suptitle(r'Marginalized Information', fontsize=15)
+legend = pl.legend(loc=0, shadow=True)
+frame = legend.get_frame()
+fig = pl.gcf()
+fig.set_size_inches(15,10)
+pl.savefig("marginal.pdf")
